4235e5ca42383f48e69be8011567c551b061f552
zhouziyang@172.18.166.69 <zhouzy36@mail2.sysu.edu.cn>
2024-05-09 23:39:23 +0800

    upload train.py utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,30 @@ import numpy as np
 import torch
 import random
 
+from dataset import TubeDataset
+from model import MPNN
+
 def setup_seed(seed):
     torch.manual_seed(seed)  # CPU
     torch.cuda.manual_seed_all(seed)  # GPU
     np.random.seed(seed)  # numpy
     random.seed(seed)  # random and transforms
     torch.backends.cudnn.deterministic = True  # cudnn
+
+
+def get_dataset(args):
+    if args["flow_name"] == "tube":
+        train_dataset = TubeDataset(filename="tube_train.hdf5", **args["dataset"])
+        val_dataset = TubeDataset(filename="tube_dev.hdf5", **args["dataset"])
+        test_dataset = TubeDataset(filename="tube_test.hdf5", **args["dataset"])
+    else:
+        raise NotImplementedError
+    return train_dataset, val_dataset, test_dataset
+
+
+def get_model(args):
+    if args["model_name"] == "mpnn":
+        model = MPNN(**args["model"])
+    else:
+        raise NotImplementedError
+    return model
